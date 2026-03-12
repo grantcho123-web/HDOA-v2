5d6544c3e1a0256eb784b7e2ec61306bbbcdf405
grantcho123-web <grantcho123@gmail.com>
2026-03-12 15:39:19 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ textAlign:'center', padding:60 }}>
+      <h2 style={{ fontSize:24, fontWeight:700, color:'white', marginBottom:12 }}>Human Data Ops v2</h2>
+      <p style={{ fontSize:14, color:'#a1a1aa' }}>Auth is live. Click Sign In above to test.</p>
+    </div>
+  );
+}
